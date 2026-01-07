749e797b3e941b7c007be02ee3ac5e1a80febfb2
Penguin-SAMA <496107146@qq.com>
2026-01-07 11:03:09 +0800

    Add submodule: spdlog

--- Hazel/xmake.lua
+++ Hazel/xmake.lua
@@ -6,6 +6,7 @@ set_kind("shared")
 add_defines("HZ_BUILD_DLL")
 
 add_includedirs("src", { public = true })
+add_includedirs("../Hazel/vendor/spdlog/include", { public = true })
 add_files("src/**.cpp")
 add_headerfiles("src/**.h", "src/**.hpp")
 
